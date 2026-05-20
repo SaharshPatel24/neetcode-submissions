4d38c3676ccbcc67036e1bc9f8094a856bfc4051
Saharsh Patel <96026220+SaharshPatel24@users.noreply.github.com>
2026-05-20 07:55:02 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums: number[]): number {
+        let curr = nums[0];
+        let best = nums[0];
+
+        for (let i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], nums[i] + curr);
+            best = Math.max(best, curr);
+        }
+
+        return best;
+    }
+}
